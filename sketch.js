47f16f5b8cd8322ab50bd76188e13ed7b5c99eed
Lachosk <1006009500@u.icesi.edu.co>
2022-03-21 17:27:07 -0500

    carga imagen de prueba

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+let img;
+  
+function preload() {
+  img = loadImage('1366_2000.png');
+}
+  
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1366, 799);
+  
+  image(img, 0,0);
+}
+
+
+
+
+
+/*function setup() {
+
 }
 
 function draw() {
-  background(220);
-}
+
+}*/
