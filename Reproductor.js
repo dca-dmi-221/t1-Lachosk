c412441f67920fa9eecbde8b5f433ec087daccf6
Lachosk <1006009500@u.icesi.edu.co>
2022-03-21 20:23:53 -0500

    Agrega boton y cancion funcional

--- Reproductor.js
+++ Reproductor.js
@@ -1,5 +1,31 @@
-class Reproductor{
-    constructor(){
-        
+class Reproductor {
+    constructor() {
+        this.cancion;
+        this.r = 255;
+        this.g = 0;
+        this.b = 0;
+    }
+
+    cargarCancion() {
+        this.cancion = loadSound('Bound_2.mp3')
+    }
+
+    pintarBoton() {
+        fill(this.r, this.g, this.b);
+        noStroke();
+        rect(600, 300, 100, 100);
+    }
+
+    hizoClick() {
+        if (this.cancion.isPlaying()) {
+            this.cancion.pause();
+            this.r = 255;
+            this.g = 0;
+
+        } else {
+            this.cancion.play();
+            this.g = 255;
+            this.r = 0;
+        }
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,23 +1,21 @@
 interfaz = new Interfaz;
-  
+reproductor = new Reproductor;
+
 function preload() {
   interfaz.cargarImagen();
 }
-  
+
 function setup() {
   createCanvas(1366, 799);
   interfaz.pintarImagen();
- 
-}
-
-
-
-
-
-/*function setup() {
-
+  reproductor.cargarCancion();
 }
 
 function draw() {
+  reproductor.pintarBoton();
+}
 
-}*/
+
+function mouseClicked() {
+  reproductor.hizoClick();
+}
